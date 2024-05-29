b8f46649876ce00c1fe03e871a1eca8d768da747
AntosFalkos <64736167+AntosFalkos@users.noreply.github.com>
2024-05-30 00:16:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, request, jsonify
-from model.py import Perceptron
 import numpy as np
+
+class Perceptron:
+    def __init__(self, input_size, lr=0.1, epochs=100):
+        self.W = np.zeros(input_size + 1)
+        self.lr = lr
+        self.epochs = epochs
+
+    def activation_fn(self, x):
+        return 1 if x >= 0 else 0
+
+    def predict(self, x):
+        z = self.W.T.dot(np.insert(x, 0, 1))
+        return self.activation_fn(z)
+
+    def fit(self, X, d):
+        for _ in range(self.epochs):
+            for i in range(d.shape[0]):
+                x = np.insert(X[i], 0, 1)
+                y = self.activation_fn(self.W.T.dot(x))
+                self.W = self.W + self.lr * (d[i] - y) * x
 
 app = Flask(__name__)
 
+# Initializing a perceptron with 2 inputs
 perceptron = Perceptron(input_size=2)
 
 @app.route('/train', methods=['POST'])
@@ -22,5 +42,4 @@ def predict():
     return jsonify({"predictions": predictions})
 
 if __name__ == '__main__':
-    train_model()
     app.run(host='0.0.0.0', port=5001)
